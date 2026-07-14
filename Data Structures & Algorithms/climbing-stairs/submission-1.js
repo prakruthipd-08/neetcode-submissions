a895a9f6ca9660ac683082442816eb35af780cc0
Prakruthi P D <prakruthipdwork@gmail.com>
2026-07-14 17:58:31 +0530

    Update: climbing-stairs - submission-1

--- Data Structures & Algorithms/climbing-stairs/submission-1.js
+++ Data Structures & Algorithms/climbing-stairs/submission-1.js
@@ -1,13 +1,16 @@
 class Solution {
-    
+    /**
+     * @param {number} n
+     * @return {number}
+     */
     climbStairs(n) {
-        const noOfWays=[1,2]
+        const noOfWays=[1,2];
         for(let i=2;i<n;i++){
-            noOfWays[i]=noOfWays[i-1]+noOfWays[i-2];
+            noOfWays[i]= noOfWays[i-1] + noOfWays[i-2];
+
         }
-    
-    return noOfWays[n-1];
+        return noOfWays[n-1]
     }
 }
-let sol=new Solution
-console.log(sol.climbStairs(2))
+let ans=new Solution()
+console.log(ans.climbStairs(2))
